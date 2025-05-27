814b3affdefbb9738b714c9478e8bd81151df1dc
cordis-dev <darius@adroiti.com>
2025-05-27 11:33:57 +0300

    Fix indentation to tabs

--- testdata/cognitive_complexity_default.go
+++ testdata/cognitive_complexity_default.go
@@ -9,5 +9,11 @@ func l() { // MATCH /function l has cognitive complexity 8 (> max enabled 7)/
 			total += i
 		}
 	}
+	if total > 0 { // +1
+		total--
+	}
+	if total > 0 && max > 0 { // +2
+		total++
+	}
 	return total && max
 }
